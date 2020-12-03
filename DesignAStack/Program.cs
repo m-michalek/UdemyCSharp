8cf02664d8baed351b4ca2b4e3af75fb9ddc80f7
Max Michalek <max.michalek@maibornwolff.de>
2020-12-03 14:28:08 +0100

    first commit

---
+++ DesignAStack/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DesignAStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var stack = new Stack();
+                
+                stack.Push(1);
+                stack.Push(2);
+                stack.Push(3);
+                
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+                Console.WriteLine(stack.Pop());
+                
+                stack.Push(1);
+                stack.Push(2);
+                stack.Push(3);
+                
+                stack.Clear();
+
+                Console.WriteLine(stack.Count());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
