312e9dea950a2089981bbd3d456af259aec01cf7
Max Michalek <max.michalek@maibornwolff.de>
2020-12-17 14:04:10 +0100

    Added example for method extensions

---
+++ MethodExtensions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MethodExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "Hello World!";
+
+            // Microsoft recommends to only use method extension if there is no other way...
+            // Try to avoid them, they can lead to unexpected behavior if the extended method gets added to the class
+            // at some time later.
+            text.Print();
+            text.PrintWithEmoji("👌");
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static void Print(this string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public static void PrintWithEmoji(this string text, string emoji)
+        {
+            Console.WriteLine($"{text} {emoji}");
+        }
+    }
+}
